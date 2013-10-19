a0cfee2008f91cc0f687bc7d83b0782aedb867ff
Yu-Ann Chen <red8012@gmail.com>
2013-10-19 23:45:04 +0800

    replace spaces with tab

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var resize_window = function(){
-  $('#page1 , #page2').css('height',$(window).height() + 'px');
+	$('#page1 , #page2').css('height',$(window).height() + 'px');
 }
 jQuery(function($){
-  $('#topage2').click(function(){
-   $('html, body').animate({scrollTop: $('#page2').offset().top} , 800);
- });
-  $(window).resize(resize_window);
-    resize_window();
+	$('#topage2').click(function(){
+		$('html, body').animate({scrollTop: $('#page2').offset().top} , 800);
+	});
+	$(window).resize(resize_window);
+	resize_window();
 });
